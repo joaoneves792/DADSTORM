ff1f4c6c000eec5e90dba636cfd8d253040db91b
luisribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2016-11-17 23:26:25 +0000

    Implemented best effort broadcast algorithm

---
+++ DistributedAlgoritmsClassLibrary/BasicBroadcast.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedAlgoritmsClassLibrary
+{
+    using Message = Object;
+
+    public class BasicBroadcast : BestEffortBroadcast {
+        private Action<Process, Message> _listener;
+        private PerfectPointToPointLink _perfectPointToPointLink;
+
+        private IProducerConsumerCollection<Process> _processes;
+
+        public BasicBroadcast(Process process, Action<Process, Message> listener) {
+            _listener = listener;
+            _perfectPointToPointLink = new EliminateDuplicates(process, Deliver);
+
+            _processes = new ConcurrentBag<Process>();
+        }
+
+        public BasicBroadcast(Process process, Action<Process, Message> listener, params Process[] otherProcesses)
+        {
+            _listener = listener;
+            _perfectPointToPointLink = new EliminateDuplicates(process, Deliver, otherProcesses);
+
+            _processes = new ConcurrentBag<Process>();
+            foreach (Process otherProcess in otherProcesses) {
+                _processes.TryAdd(otherProcess);
+            }
+        }
+
+        public void Broadcast(Message message) {
+            foreach (Process process in _processes) {
+                _perfectPointToPointLink.Send(process, message);
+            }
+        }
+
+        public void Deliver(Process process, Message message) {
+            _listener(process, message);
+        }
+
+        public void Connect(Process process) {
+            _perfectPointToPointLink.Connect(process);
+            _processes.TryAdd(process);
+        }
+    }
+}
---
+++ DistributedAlgoritmsClassLibrary/BestEffortBroadcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedAlgoritmsClassLibrary
+{
+    using Message = Object;
+
+    public interface BestEffortBroadcast
+    {
+        void Broadcast(Message message);
+        void Deliver(Process process, Message message);
+        void Connect(Process process);
+    }
+}
