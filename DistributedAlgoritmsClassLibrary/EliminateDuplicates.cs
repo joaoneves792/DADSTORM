410c51b13d30af43c0f9a6674c9ee0266064ce27
luisribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2016-11-17 23:25:53 +0000

    Implemented perfect point to point link algorithm

---
+++ DistributedAlgoritmsClassLibrary/EliminateDuplicates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedAlgoritmsClassLibrary {
+    using Message = Object;
+
+    public class EliminateDuplicates : PerfectPointToPointLink {
+        private Action<Process, Message> _listener;
+        private StubbornPointToPointLink _stubbornPointToPointLink;
+
+        private IProducerConsumerCollection<Tuple<Process, Message>> _delivered;
+
+        public EliminateDuplicates(Process process, Action<Process, Message> listener) {
+            _listener = listener;
+            _stubbornPointToPointLink = new RetransmitForever(process, Deliver);
+
+            _delivered = new ConcurrentBag<Tuple<Process, Message>>();
+        }
+
+        public EliminateDuplicates(Process process, Action<Process, Message> listener, params Process[] otherProcesses) {
+            _listener = listener;
+            _stubbornPointToPointLink = new RetransmitForever(process, Deliver, otherProcesses);
+
+            _delivered = new ConcurrentBag<Tuple<Process, Message>>();
+        }
+
+        public void Send(Process process, Message message) {
+            _stubbornPointToPointLink.Send(process, message);
+        }
+
+        public void Deliver(Process process, Message message) {
+            Tuple<Process, Message> delivered = new Tuple<Process, Message>(process, message);
+
+            if (!_delivered.Contains(delivered)) {
+                _delivered.TryAdd(delivered);
+                _listener(process, message);
+            }
+        }
+
+        public void Connect(Process process) {
+            _stubbornPointToPointLink.Connect(process);
+        }
+    }
+}
