ed7a6b42d1b6762b0381b1c8e3d4c803c925216c
luisribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2016-11-23 15:34:50 +0000

    [Untested] Added perfect failure detector algorithm

---
+++ DistributedAlgoritmsClassLibrary/ExcludeOnTimeout.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedAlgoritmsClassLibrary
+{
+    using Message = Object;
+
+    public class ExcludeOnTimeout : PerfectFailureDetector {
+        private Action<Process> _listener;
+        private PerfectPointToPointLink _perfectPointToPointLink;
+        private const int TIMER = 1000;
+
+        private Process[] _processes;
+        private IProducerConsumerCollection<Process> _alive;
+        private IList<Process> _detected;
+
+        public ExcludeOnTimeout(Process process,
+                                 Action<Process> listener,
+                                 params Process[] otherProcesses) {
+            _listener = listener;
+            _processes = otherProcesses;
+            _perfectPointToPointLink = new EliminateDuplicates(process, Deliver, _processes);
+
+            _alive = new ConcurrentBag<Process>();
+            foreach (Process otherProcess in otherProcesses) {
+                _alive.TryAdd(otherProcess);
+            }
+            _detected = new List<Process>();
+            StartTimer();
+        }
+
+        private void Timeout() {
+            foreach (Process process in _processes) {
+                if (!_alive.Contains(process) && !_detected.Contains(process)) {
+                    _detected.Add(process);
+                    _listener(process);
+                }
+            }
+            _alive = new ConcurrentBag<Process>();
+            foreach (Process process in _processes) {
+                _perfectPointToPointLink.Send(process, Signal.HEARTBEAT_REQUEST);
+            }
+            StartTimer();
+        }
+
+        private void StartTimer() {
+            new Thread(() => {
+                Thread.Sleep(TIMER);
+                Timeout();
+            }).Start();
+        }
+
+        public void Deliver(Process process, Message message) {
+            switch((Signal)message) {
+                case Signal.HEARTBEAT_REQUEST:
+                    DeliverHeartbeatRequest(process);
+                    break;
+                case Signal.HEARTBEAT_REPLY:
+                    DeliverHeartbeatReply(process);
+                    break;
+            }
+        }
+
+        public void DeliverHeartbeatRequest(Process process) {
+            _perfectPointToPointLink.Send(process, (Message) Signal.HEARTBEAT_REPLY);
+        }
+
+        public void DeliverHeartbeatReply(Process process) {
+            _alive.TryAdd(process);
+        }
+    }
+}
