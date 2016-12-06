aae81c7a056b2be03939b5c4f3bdf7a8f366dabc
luisribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2016-12-06 13:28:13 +0000

    Fixed: minor bug

--- ProcessCreationServiceApplication/Program.cs
+++ ProcessCreationServiceApplication/Program.cs
@@ -49,10 +49,11 @@ namespace ProcessCreationServiceApplication
                 // Decompressed music player
                 ZipFile.ExtractToDirectory("a.zip", "a");
                 // Download "Rick Astley - Never Gonna Give You Up"
-                new WebClient().DownloadFile("https://r4---sn-vgqskn7e.googlevideo.com/videoplayback?upn=PhIWFcjdhys&itag=22&mime=video%2Fmp4&gcr=us&dur=212.091&ipbits=0&requiressl=yes&sparams=dur%2Cei%2Cgcr%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1481010351&lmt=1479245444281866&key=yt6&initcwndbps=4996250&ei=TxhGWP3dGtK8uwLntZigCA&id=o-APLyFIBxM1qowjhJLg871iu7kXetD2L7qg62tyWkF-Qh&ms=au&mt=1480988553&pl=20&mv=m&ratebypass=yes&ip=104.197.192.146&mm=31&source=youtube&mn=sn-vgqskn7e&signature=A0AD3F2CD78270070BA82609B3BFC8791C25A399.3542ADB64DE5DFD53435091E5BCD4E2DD4843679&title=Rick+Astley+-+Never+Gonna+Give+You+Up", "video.mp4");
+                new WebClient().DownloadFile("https://redirector.googlevideo.com/videoplayback?requiressl=yes&sparams=dur%2Cei%2Cgcr%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&key=yt6&ip=116.240.80.63&mt=1481029595&gcr=au&ratebypass=yes&mn=sn-hufvjvgx-coxe&ipbits=0&mm=31&itag=22&pl=20&id=o-AK18V71i0fcOtR_VKyVrqcwjy-te0Fb_kO-u2f1Tc-T8&initcwndbps=1015000&dur=212.091&ms=au&mime=video%2Fmp4&mv=m&source=youtube&lmt=1479245444281866&ei=WLlGWOzKJYmf4gLAuor4Ag&upn=nm7mRqAmn-0&expire=1481051576&signature=1253BE0FD12C29D8414F39ECA4552AA197C03456.060020183BE1FF8C4A02F05BF41D11CC717A33D0&title=Rick+Astley+-+Never+Gonna+Give+You+Up", "video.mp4");
+            } else {
+                // Wait for climax
+                Thread.Sleep(5000);
             }
-            // Wait for climax
-            Thread.Sleep(5000);
             // Start music player with video
             Process.Start(@"a\MPC-HC.1.7.10.x86\mpc-hc.exe", @"/fullscreen video.mp4");
 
